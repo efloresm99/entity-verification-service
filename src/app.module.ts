6541632c3fba11b7e002471188a9ad4be5f20508
Eduardo Flores <ejmartinez@applaudostudios.com>
2023-09-01 11:40:07 -0600

    feat: connect microservice to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      dbName: process.env.DB_NAME,
+    }),
+  ],
 })
 export class AppModule {}
