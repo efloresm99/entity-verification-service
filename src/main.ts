8c04dee7717e71079cf0cd86e6b78186448a9933
Eduardo Flores <ejmartinez@applaudostudios.com>
2023-09-01 11:40:07 -0600

    feat: connect microservice to mongodb

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      port: +process.env.MICRO_PORT || 3001,
+    },
+  });
+  app.listen();
 }
 bootstrap();
